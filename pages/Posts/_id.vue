06f070f9efabc616710fc33d43684e008d20e7a1
kientt6602 <kientt6602@co-well.vn>
2023-08-08 11:34:46 +0700

    DELETE a post

--- pages/Posts/_id.vue
+++ pages/Posts/_id.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        {{post}}
+        <div>{{post}}</div>
+        <div>
+            <button type="submit" @click="removeData">delete</button>
+        </div>
     </div>
 </template>
 
@@ -19,6 +22,21 @@ export default {
         const id = params.id;
         const post = await $api.$get(`/posts/${id}.json`);
         return {post}
+    },
+    created() {
+        console.log(this.$route.params.id)
+    },
+    methods: {
+        async removeData() {
+            if(!confirm('Ban co muon xoa data nay chu?')) return;
+            const DELETE_URL = `posts/${this.$route.params.id}.json`;
+            const result = await this.$api.$delete(DELETE_URL).then(rs => alert('success')).catch(e => {
+                alert(e)
+                console.log(e)
+            })
+            console.log('result: ', result)
+            //navigate to '/Posts'
+        }
     },
 }
 </script>
--- plugins/api.js
+++ plugins/api.js
@@ -5,6 +5,9 @@ export default function ({ $axios }, inject) {
             common: {
                 Accept: 'text/plain, */*'
             }
+        },
+        params: {
+            auth: "eyJhbGciOiJSUzI1NiIsImtpZCI6ImNmM2I1YWRhM2NhMzkxNTQ4ZDM1OTJiMzU5MjkyM2UzNjAxMmI5MTQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZmlyLWxvZ2luLWQyMTJkIiwiYXVkIjoiZmlyLWxvZ2luLWQyMTJkIiwiYXV0aF90aW1lIjoxNjkxNDY1NjM3LCJ1c2VyX2lkIjoiQndscGUyTURLcWIzQXJ3Z1hPMzFFUG11NEZKMiIsInN1YiI6IkJ3bHBlMk1ES3FiM0Fyd2dYTzMxRVBtdTRGSjIiLCJpYXQiOjE2OTE0NjU2MzcsImV4cCI6MTY5MTQ2OTIzNywiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiYWRtaW5AZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.K2xetMWlD2qdkdcENIBpmxUTnuckl08Z3ZEswwvopOEKcEvvDwYRkz7828VCZex9lw4Urm1MWdaIzNf4Qebt3EOrloS1FmOqoOd_fsUHIjqKeod_JuQM15yajwRHJkcm0Qjj7YQ0SNjc1IA4THuAfMd-lYg4VVOwQuv8QKB8bT9tWo73CnwiP7PP90CthgYfPORDfjkFFGHib7vfOwLFV4hcuWxFmZsdFHtReQytQo1YhjMv8x4YBt9_VdzXM1SpJy_gGsKHbLqjuDSKDNtYlWf_woS1Y05pcc8CPYNDx90GE4Z0zSkJCA_S6HklanHpzNzk-gNPP_Y41mb3zyk-2w"
         }
     })
 

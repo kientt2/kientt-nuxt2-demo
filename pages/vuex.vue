4747532ec3ef02148c9be6776286dfa78385daf9
kientt6602 <kientt6602@co-well.vn>
2023-08-09 16:18:34 +0700

    Simple store test https://vuex.vuejs.org/guide/state.html

---
+++ pages/vuex.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h1>Vuex store demo</h1>
+        <div>Count: {{ count }}</div>
+        <div>Step: <input type="text" v-model="step"></div>
+        <div>
+            <button @click="handleIncrease">Increase</button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import {mapState, mapGetters} from 'vuex';
+
+export default {
+    data() {
+        return {
+            step: 5,
+        }
+    },
+    created() {},
+    methods: {
+        handleIncrease() {
+            this.$store.commit('increment', this.step);
+        }
+    },
+    computed: mapState({
+        count: state => state.count,
+        countPlusLocalState(state) {
+            return state.count + this.localCount
+        },
+    }),
+
+    // computed: mapState({
+    //     count: state => state.count,
+    //     countPlusLocalState: state => state.count + this.localCount
+    // }) // error
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
